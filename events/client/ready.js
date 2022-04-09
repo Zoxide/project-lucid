23805d0188b328c537a5c1818b64eab67bc5fa19
Zoxide <zoxidemc@gmail.com>
2022-04-09 18:38:44 +0100

    Updated ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "ready",
     async execute(client) {
         console.log(`[!] ${client.user.tag} is now Online!`.red.underline)
-        console.log('Made By V8.#6760'.trap.red)
+        console.log('Maintained By V8.#6760 & Aidan.#0002'.trap.red)
         client.user.setActivity('Project Lucid')
     }
 }
