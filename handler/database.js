db4fe3878293b7340b324efa4e178360b1d2ac3f
Zoxide <zoxidemc@gmail.com>
2022-04-09 18:30:44 +0100

    Added mysql manager (connection.js)

---
+++ handler/database.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+const config = require('../config.json');7
+
+const connection = mysql.createConnection(config.database);
+
+connection.connect(function (error) {
+    if (error) return console.error(error);
+
+    console.log(`\x1b[35mLOADER \x1b[37m» \x1b[35mSuccessfully connected to the database`);
+});
+
+
+module.exports = connection;
